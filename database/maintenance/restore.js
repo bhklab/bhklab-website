ba30c7d1ea8d6b265ce505387c703414c22b8dce
parinaznasr <parinaz.nasresfahani@gmail.com>
2023-03-28 23:20:27 -0400

    Added login

--- database/maintenance/restore.js
+++ database/maintenance/restore.js
@@ -38,79 +38,80 @@ const restore = async (file, Model) => {
 
 (async () => {
     try {
+        mongoose.set("strictQuery", false);
         await mongoose.connect(process.env.DEV, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
         console.log("connection open");
         //
-        // // insert members
-        // await restore("./data/members.json", Member);
-        // console.log("members done");
-        //
-        // // insert news
-        // await restore("./data/news.json", News);
-        // console.log("news done");
-        //
-        // // insert awards
-        // await restore("./data/awards.json",Award );
-        // console.log("awards done");
-        //
-        // // insert conference
-        // await restore("./data/conferences.json", Conference);
-        // console.log("conference done");
-        //
-        // // insert poster
-        // await restore("./data/posters.json", Poster);
-        // console.log("poster done");
-        //
-        // insert research
-        // await restore("./data/researches.json", Research);
-        // console.log("research done");
+        // insert members
+        await restore("./data/members.json", Member);
+        console.log("members done");
 
-        // // insert position
-        // await restore("./data/positions.json", Position);
-        // console.log("position done");
-        //
-        // // insert other accomplishments
-        // await restore("./data/otheraccoms.json", Otheraccom);
-        // console.log("otheraccoms done");
-        //
-        // // insert grants
-        // await restore("./data/grants.json", Grant);
-        // console.log("grants done");
-        //
-        // // insert datasets
-        // await restore("./data/datasets.json", Dataset);
-        // console.log("datasets done");
-        //
-        // // insert socials
-        // await restore("./data/socials.json", Social);
-        // console.log("socials done");
-        //
-        // // insert projects
-        // await restore("./data/projects.json", Project);
-        // console.log("projects done");
-        //
-        // // insert supervisions
-        // await restore("./data/supervisions.json", Supervision);
-        // console.log("supervisions done");
-        //
-        // // insert project indexes
-        // await restore("./data/indexes.json", Index);
-        // console.log("indexes done");
-        //
-        // // insert memberProject
-        // await restore("./data/memberprojects.json", Memberproject);
-        // console.log("memberprojects done");
-        //
-        // // // insert lab publications
-        // await restore("./data/publications.json", Publication);
-        // console.log("publications done");
-        //
-        // // // insert lab publications
-        // await restore("./data/presentations.json", Presentation);
-        // console.log("presentations done");
+        // insert news
+        await restore("./data/news.json", News);
+        console.log("news done");
+
+        // insert awards
+        await restore("./data/awards.json",Award );
+        console.log("awards done");
+
+        // insert conference
+        await restore("./data/conferences.json", Conference);
+        console.log("conference done");
+
+        // insert poster
+        await restore("./data/posters.json", Poster);
+        console.log("poster done");
+
+        //insert research
+        await restore("./data/researches.json", Research);
+        console.log("research done");
+
+        // insert position
+        await restore("./data/positions.json", Position);
+        console.log("position done");
+
+        // insert other accomplishments
+        await restore("./data/otheraccoms.json", Otheraccom);
+        console.log("otheraccoms done");
+
+        // insert grants
+        await restore("./data/grants.json", Grant);
+        console.log("grants done");
+
+        // insert datasets
+        await restore("./data/datasets.json", Dataset);
+        console.log("datasets done");
+
+        // insert socials
+        await restore("./data/socials.json", Social);
+        console.log("socials done");
+
+        // insert projects
+        await restore("./data/projects.json", Project);
+        console.log("projects done");
+
+        // insert supervisions
+        await restore("./data/supervisions.json", Supervision);
+        console.log("supervisions done");
+
+        // insert project indexes
+        await restore("./data/indexes.json", Index);
+        console.log("indexes done");
+
+        // insert memberProject
+        await restore("./data/memberprojects.json", Memberproject);
+        console.log("memberprojects done");
+
+        // // insert lab publications
+        await restore("./data/publications.json", Publication);
+        console.log("publications done");
+
+        // // insert lab publications
+        await restore("./data/presentations.json", Presentation);
+        console.log("presentations done");
 
 
     } catch (err) {
