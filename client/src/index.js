7b93ffdf7aac29e83a65a03522759dd62eb2d9d7
gangesh <gangeshberi@gmail.com>
2023-04-26 14:58:33 -0400

    added eslint to the FE codebase

--- client/src/index.js
+++ client/src/index.js
@@ -8,27 +8,27 @@ import axios from 'axios';
 
 //Add a request interceptor
 axios.interceptors.request.use(
-    config => {
-        const token = window?.accessToken || 'hello';
-        if (token) {
-            config.headers['Authorization'] = 'Bearer ' + token;
-        }
+	config => {
+		const token = window?.accessToken || 'hello';
+		if (token) {
+			config.headers['Authorization'] = 'Bearer ' + token;
+		}
 
-        return config;
-    },
-    error => {
-        Promise.reject(error)
-    }
+		return config;
+	},
+	error => {
+		Promise.reject(error);
+	}
 );
 
 // Add a response interceptor
 axios.interceptors.response.use(
-    response => {
-        return response;
-    },
-    error => {
-        Promise.reject(error)
-    }
+	response => {
+		return response;
+	},
+	error => {
+		Promise.reject(error);
+	}
 );
 
 const root = createRoot(document.getElementById('root'));
