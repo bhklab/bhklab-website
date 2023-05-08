ad5872eb0b82f4dd6715005698c738aaf54a713b
gangesh <gangeshberi@gmail.com>
2023-05-08 13:54:29 -0400

    minor change to global styles file

--- client/src/styles/GlobalStyles.js
+++ client/src/styles/GlobalStyles.js
@@ -10,7 +10,7 @@ const GlobalStyles = createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
-        font-family: 'Ralway', 'Roboto', 'Open Sans', Arial, Helvetica, sans-serif;
+        font-family: 'Roboto', 'Raleway', 'Open Sans', Arial, Helvetica, sans-serif;
     }
 
     body {
