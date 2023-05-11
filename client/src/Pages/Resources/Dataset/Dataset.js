bfef769616e69beffb97ccd238578d7341569e44
gangesh <gangeshberi@gmail.com>
2023-05-11 00:13:30 -0400

    updated Dataset page to improve the UI design

--- client/src/Pages/Resources/Dataset/Dataset.js
+++ client/src/Pages/Resources/Dataset/Dataset.js
@@ -1,8 +1,8 @@
+/* eslint-disable no-underscore-dangle */
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import parse from 'html-react-parser';
-
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -13,25 +13,25 @@ import Paper from '@mui/material/Paper';
 import styled from 'styled-components';
 import Container from '@mui/material/Container';
 import Layout from '../../../Components/Utils/Layout';
+import StyledHeading from '../../../styles/StyledHeading';
+import colors from '../../../styles/colors';
 
-const Banner = styled.div`
-	width: 100%;
-	height: 150px;
-	background-color: white;
-	padding: 50px 150px;
-	display: flex;
-	flex-direction: column;
+const StyledContainer = styled.div`
+	color: ${colors.primary_text_color};
+	margin-bottom: 80px;
 
 	.text {
-	display: flex;
-	justify-content: center;
-	color: black;
+		margin: 20px 0;
+	}
+
+	td {
+		color: ${colors.primary_text_color} !important;
 	}
 `;
 
 const accessions = (items) => (
-	items.map((item, index) => (
-		<div className="content" key={index}>
+	items.map((item) => (
+		<div className="content" key={item.name}>
 			<a href={item.link} target="_blank" rel="noreferrer">{item.name}</a>
 			<br />
 		</div>
@@ -54,7 +54,6 @@ function Dataset() {
 
 	useEffect(() => (() => {
 		if (history.action === 'POP' && history.location.pathname === '/') {
-			console.log('history');
 			history.replace({
 				pathname: '/',
 				state: {
@@ -65,47 +64,54 @@ function Dataset() {
 
 	return (
 		<Layout>
-			<Banner>
+			<StyledContainer>
+				<StyledHeading>
+					Datasets
+				</StyledHeading>
 				<Container>
 					<div className="text">
-						Data generated/curated as part of our research are shared via public repositories such as NCBI Gene Expression Omnibus or data packages. My lab maintains the following datasets:
+						Data generated/curated as part of our research are shared via public repositories
+						such as NCBI Gene Expression Omnibus or data packages.
+						The lab maintains the following datasets:
 					</div>
 				</Container>
-			</Banner>
-			<Container>
-				{
-					ready
-					&& (
-						<TableContainer component={Paper}>
-							<Table sx={{ minWidth: 650 }} aria-label="simple table">
-								<TableHead>
-									<TableRow>
-										<TableCell>Accession</TableCell>
-										<TableCell align="left">Title</TableCell>
-										<TableCell align="right">#</TableCell>
-										<TableCell align="right">Release</TableCell>
-									</TableRow>
-								</TableHead>
-								<TableBody>
-									{datasets.map((row) => (
-										<TableRow
-											key={row._id}
-											sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-										>
-											<TableCell component="th" scope="row">
-												{accessions(row.accession)}
-											</TableCell>
-											<TableCell align="left">{parse(row.title)}</TableCell>
-											<TableCell align="right">{row.samples}</TableCell>
-											<TableCell align="right">{row.release}</TableCell>
+				<Container>
+					{
+						ready
+						&& (
+							<TableContainer component={Paper}>
+								<Table sx={{ minWidth: 650 }} aria-label="simple table">
+									<TableHead>
+										<TableRow>
+											<TableCell>Accession</TableCell>
+											<TableCell align="left">Title</TableCell>
+											<TableCell align="right">#</TableCell>
+											<TableCell align="right">Release</TableCell>
 										</TableRow>
-									))}
-								</TableBody>
-							</Table>
-						</TableContainer>
-					)
-				}
-			</Container>
+									</TableHead>
+									<TableBody>
+										{
+											datasets.map((row) => (
+												<TableRow
+													key={row._id}
+													sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+												>
+													<TableCell component="th" scope="row">
+														{accessions(row.accession)}
+													</TableCell>
+													<TableCell align="left">{parse(row.title)}</TableCell>
+													<TableCell align="right">{row.samples}</TableCell>
+													<TableCell align="right">{row.release}</TableCell>
+												</TableRow>
+											))
+										}
+									</TableBody>
+								</Table>
+							</TableContainer>
+						)
+					}
+				</Container>
+			</StyledContainer>
 		</Layout>
 	);
 }
