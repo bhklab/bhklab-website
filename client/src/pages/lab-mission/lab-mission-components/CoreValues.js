7034faba9cff670908ed5aaa15956e7f580a0825
gangesh <gangeshberi@gmail.com>
2023-07-07 21:47:46 -0400

    updated CoreValues component to include new images, alt attribute

--- client/src/pages/lab-mission/lab-mission-components/CoreValues.js
+++ client/src/pages/lab-mission/lab-mission-components/CoreValues.js
@@ -2,22 +2,39 @@ import React from 'react';
 import { StyledValues } from '../LabMissionStyles';
 
 const coreValuesList = {
-	Openness: 'Research transparency, reproducibility, and reusability',
-	Accessibility: 'Science must be accessible and beneficial to everyone',
-	Multidisciplinarity: 'Our research is fundamentally multidisciplinary and all expertise is valued',
-	Diversity:
-	<>
-		Recognition and valorization of diverse contributions
-		<a
-			href="https://sfdora.org/"
-			target="_blank"
-			rel="noreferrer"
-		>
-			{' '}
-			Declaration on Research Assessment principles
-			{' '}
-		</a>
-	</>,
+	Openness: {
+		image: '/images/Logo/customer.png',
+		description: 'Research transparency, reproducibility, and reusability',
+		altAttribute: 'reproducibility',
+	},
+	Accessibility: {
+		image: '/images/Logo/accessibility.png',
+		description: 'Science must be accessible and beneficial to everyone',
+		altAttribute: 'accessibility',
+	},
+	Multidisciplinarity: {
+		image: '/images/Logo/multidisciplinarity.png',
+		description: 'Our research is fundamentally multidisciplinary and all expertise is valued',
+		altAttribute: 'multi-disciplinarity',
+	},
+	Diversity: {
+		image: '/images/Logo/diversity-values.png',
+		description: (
+			<>
+				Recognition and valorization of diverse contributions
+				<a
+					href="https://sfdora.org/"
+					target="_blank"
+					rel="noreferrer"
+				>
+					{' '}
+					Declaration on Research Assessment principles
+					{' '}
+				</a>
+			</>
+		),
+		altAttribute: 'diversity',
+	},
 };
 
 /**
@@ -28,14 +45,14 @@ const coreValuesList = {
 const displayCoreValues = (values) => Object.entries(values).map(([key, value]) => (
 	<div className="core-values-single-list-item">
 		<img
-			src="/images/Logo/customer.png"
-			alt="customer"
+			src={value.image}
+			alt={value.altAttribute}
 		/>
 		<h4>
 			{key}
 		</h4>
 		<p>
-			{value}
+			{value.description}
 		</p>
 	</div>
 ));
