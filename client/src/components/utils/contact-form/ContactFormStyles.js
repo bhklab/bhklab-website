b7abbd65063736a28bb9a1e0d94ccb586bb2667c
gangesh <gangeshberi@gmail.com>
2023-07-29 11:00:34 -0400

    changes to contact form

--- client/src/components/utils/contact-form/ContactFormStyles.js
+++ client/src/components/utils/contact-form/ContactFormStyles.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Box from '@mui/material/Box';
+import colors from '../../../styles/colors';
 
 const StyledPIInfo = styled.div`
   width: 100%;
@@ -8,12 +9,22 @@ const StyledPIInfo = styled.div`
   flex-direction: column;
   justify-content: left;
   height: 80px;
-  line-spacing: 20px;
+  line-height: 20px;
   margin-left: 5px;
   margin-bottom: 10px;
   font-size: 16px;
   font-weight: normal;
   text-align: left;
+
+  .pi-name {
+	font-weight: 700;
+  }
+
+  .pi-position {
+	font-size: 15px;
+	color: ${colors.dark_gray};
+  }
+
   @media only screen and (max-width: 550px) {
 	height: 100px;
   }
