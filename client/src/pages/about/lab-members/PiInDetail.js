048fd5fc92ba6cba5854976e0897adea70e55ed4
mattbocc <matthew.boccalon@live.ca>
2025-12-17 14:42:39 -0500

    feat: added PI bsky link

--- client/src/pages/about/lab-members/PiInDetail.js
+++ client/src/pages/about/lab-members/PiInDetail.js
@@ -1,13 +1,15 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import {
-	StyledPiInfoDetailed, StyledPiDetailed,
-	StyledPiNameDetailed, StyledTitleDetailed, StyledBioDetailed, StyledPiImageDetailed,
+	StyledPiInfoDetailed,
+	StyledPiDetailed,
+	StyledPiNameDetailed,
+	StyledTitleDetailed,
+	StyledBioDetailed,
+	StyledPiImageDetailed,
 } from './MembersInDetailStyles';
 
-function PiInDetail({
-	photo, name, title, bio,
-}) {
+function PiInDetail({ photo, name, title, bio }) {
 	return (
 		<StyledPiDetailed>
 			<StyledPiImageDetailed src={photo} alt={title} />
@@ -15,19 +17,22 @@ function PiInDetail({
 				<StyledPiNameDetailed>
 					<span>{name}</span>
 					<div className="social-container">
-						<a
-							href="https://twitter.com/bhaibeka"
-							target="_blank"
-							rel="noreferrer"
-						>
+						<a href="https://twitter.com/bhaibeka" target="_blank" rel="noreferrer">
 							<img src="/images/social-media/x.png" alt="twitter" style={{ maxWidth: '25px', borderRadius: '5px' }} />
 						</a>
-						<a
-							href="https://www.linkedin.com/in/benhaibekains/"
-							target="_blank"
-							rel="noreferrer"
-						>
-							<img src="/images/social-media/linkedin-icon.png" alt="linkedin" style={{ maxWidth: '25px', borderRadius: '5px' }} />
+						<a href="https://www.linkedin.com/in/benhaibekains/" target="_blank" rel="noreferrer">
+							<img
+								src="/images/social-media/linkedin-icon.png"
+								alt="linkedin"
+								style={{ maxWidth: '25px', borderRadius: '5px' }}
+							/>
+						</a>
+						<a href="https://bsky.app/profile/bhaibeka.bsky.social" target="_blank" rel="noreferrer">
+							<img
+								src="/images/social-media/bluesky-icon.png"
+								alt="bluesky"
+								style={{ maxWidth: '25px', borderRadius: '5px' }}
+							/>
 						</a>
 					</div>
 				</StyledPiNameDetailed>
